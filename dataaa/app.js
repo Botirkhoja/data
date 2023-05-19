a0b8cef745be6bae0ce6b8ffdbeba7ab65f0cf45
Yunus <128239091+Botirkhoja@users.noreply.github.com>
2023-05-19 17:20:44 +0300

    Add files via upload

---
+++ dataaa/app.js
@@ -0,0 +1,93 @@
+///   Data types malumot turlari 
+
+
+// 1 String = "Ibroximjon";
+// 2 Number = 12344;
+// 3 BigInt = 1990n;
+// 4 Boolean = true / false 
+// 5 Object  = {} obyekt
+// 6 Undefined = bor lekin yoq  / Xatolar turlari
+// 7 null = umuman yoq / Xatolar turi 
+// 8 Symbol = @#$%^&*()_
+
+// 9 Function = 
+function funksiyagaNOm () {
+
+}
+
+// const name = "John";
+// console.log(name)
+
+
+
+// let const var 
+
+// const ism = "Zayniddin";
+
+// console.log(ism)
+
+
+// let son = 5;
+// let son2 = 7;
+
+// let hammasi = son + son2;
+
+// console.log(hammasi);
+
+// let son = 100009;
+// let son1 = 2345;
+
+// console.log(son - son1);
+
+
+
+
+// let a = 91;
+// let b = 90;
+
+// console.log(a < b )
+
+// let ism = "anvnar";
+//  console.log(ism == "anvar");
+
+
+
+
+// let fio = {
+//     "name": "ibroximjon",
+//     "famil" : "Muhammadjonov",
+//     "nomer" : 9978687575,
+//     "promis" : "Tashkent",
+//     "TugJoy" : "Uzbekistan",
+//     "Rasm" : "https://s0.2mdn.net/simgad/3952834936881037361"
+
+// }
+
+// console.log(fio);
+
+
+// let name1;
+
+
+
+// console.log(name1);
+
+
+// let nomer;
+
+
+// console.log(nomer);
+
+
+
+
+
+console.log(typeof(7645))
+
+
+
+
+console.log(typeof())
+
+
+
